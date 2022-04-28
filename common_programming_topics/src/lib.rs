37d1e5b8246528ec52b627d87b7532e893902281
Sky Brewer <jaco.brewer@gmail.com>
2022-04-28 15:15:22 +0200

    my first ideas of tests

--- common_programming_topics/src/lib.rs
+++ common_programming_topics/src/lib.rs
@@ -17,7 +17,43 @@ mod numbers {
 
     #[test]
     fn float_almost_equal() {
-        assert!( 0.001 > (1.0 / 3.0 - 0.3333333333333333) )
+        assert!(0.001 > (1.0 / 3.0 - 0.3333333333333333))
+    }
+}
+
+#[cfg(test)]
+mod variables {
+
+    #[test]
+    fn assign() {
+        let health = 5;
+        assert_eq!(health, 5);
     }
 
+    #[test]
+    #[allow(unused_assignments)]
+    fn mutability() {
+        let mut health = 5;
+        health = 6;
+        assert_eq!(health, 6);
+    }
+
+    // Won't work on all platforms as type names change
+    fn type_of<T>(_: &T) -> &'static str {
+        return std::any::type_name::<T>();
+    }
+
+    #[test]
+    fn variable_type() {
+        let health = 5;
+        assert_eq!(type_of(&health), "i32");
+    }
+
+    #[test]
+    fn multi_assign() {
+        let health = 5;
+        let new_health = health;
+        assert_eq!(health, new_health);
+        assert_eq!(new_health, 5);
+    }
 }
